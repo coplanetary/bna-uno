a8ebef53c3f744156a7202250e1c1448b5d4ce63
Risav Karna <vasir@fullmingo.>
2024-04-24 08:55:06 +0545

    build(packages): add blazing fast astro

---
+++ blazin-fastro/astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config';
+import lit from '@astrojs/lit';
+import preact from '@astrojs/preact';
+import svelte from '@astrojs/svelte';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { fileURLToPath } from 'node:url';
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: '../dist/blazin-fastro',
+  integrations: [
+    lit(),
+    preact(),
+    svelte(),
+    partytown(),
+    sitemap(),
+    tailwind({
+      configFile: fileURLToPath(
+        new URL('./tailwind.config.cjs', import.meta.url)
+      ),
+    }),
+  ],
+});
