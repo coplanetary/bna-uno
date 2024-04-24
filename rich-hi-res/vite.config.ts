e77749235619c49abc77dc70302da9aa96efebc8
Risav Karna <vasir@fullmingo.>
2024-04-24 09:08:33 +0545

    build(rich-harris): setup rich hi-res experiences via SvelteKit

---
+++ rich-hi-res/vite.config.ts
@@ -0,0 +1,34 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../node_modules/.vite/rich-hi-res',
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+
+  plugins: [sveltekit(), nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  build: {
+    outDir: '../dist/rich-hi-res',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
