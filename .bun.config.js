f37c8e7da002f69b94c4e6b65218d6b34fba815a
anarchitekt <risavkarna@gmail.com>
2024-05-05 20:09:33 +0545

    feat(astrowind): add component lib

---
+++ .bun.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "transpilers": [
+    {
+      "name": "typescript",
+      "entry": "tsconfig.base.json"
+    },
+    {
+      "name": "nx",
+      "entry": "node_modules/nx/package.json"
+    }
+  ]
+}
