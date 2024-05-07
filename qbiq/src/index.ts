8f9eb57f7a8b0503d23bd67e6dc8fe6cb0973d59
anarchitekt <risavkarna@gmail.com>
2024-05-07 18:33:59 +0545

    feat(prisma-hono): add prisma client meta api

--- qbiq/src/index.ts
+++ qbiq/src/index.ts
@@ -1,7 +1,11 @@
 import { Prisma } from '@prisma/client';
 import { Hono } from 'hono';
+import { PrismaClient } from './client';
 
 const app = new Hono();
+const prisma = new PrismaClient({
+  errorFormat: 'pretty'
+});
 
 app.get('/hello', (c) => {
   return c.text('Hello from QbiQ!')
@@ -10,6 +14,16 @@ app.get('/hello', (c) => {
 app.get('/meta', (c) => {
   return c.json(Prisma.dmmf.datamodel.models)
 });
+
+app.get('meta/:model', (c) => {
+  const model = c.req.param("model");
+  const refs = (prisma as any)[model]['fields'];
+  if (refs) {
+    return c.json(refs);
+  } else {
+    return c.json({});
+  }
+})
 
 
 export default app;
